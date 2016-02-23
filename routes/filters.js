926f744fb7c203caf64af6e58c076440ec085d68
Matt Chessman <matthewmatics17@gmail.com>
2016-02-23 15:02:12 -0700

    updated filter routes

--- routes/filters.js
+++ routes/filters.js
@@ -1,30 +1,63 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
+function checkErr(res, err) {
+  var fail = false;
+  if(err) {
+    fail = true;
+    res.send(err);
+  }
+  return fail;
+}
 
 /*****
 FILTERS
 *****/
 
-router.get('/filters/all', function(req, res, next) {
+router.get('/all', function(req, res, next) {
   //Get all filters associated with a user including defaults.
-  res.end('End');
+  knex('filters').where({user_id : -1}).orWhere({user_id : req.user.id})
+  .then(function(err, data) {
+    if(!checkErr(res, err)) {
+      res.json(data);
+    }
+  });
 });
 
-router.get('/filters/specific', function(req, res, next) {
+router.get('/specific', function(req, res, next) {
   //Get specific filters associated with a user.
-  res.end('End');
+  knex('filters').where({user_id : req.user.id})
+  .then(function(err, data) {
+    if(!checkErr(res, err)) {
+      res.json(data);
+    }
+  });
 });
 
-router.post('/filters', function(req, res, next) {
+router.post('/', function(req, res, next) {
   //Build new filter associated with a user.
-  res.end('End');
+  knex('filters').insert({
+    user_id: req.user.id,
+    phrase: req.body.content
+  })
+  .then(function(err, data) {
+    if(!checkErr(res, err)) {
+      res.send('success');
+    }
+  });
 });
 
-router.delete('/filters/specific/:filterID', function(req, res, next) {
+router.delete('/specific/:filterID', function(req, res, next) {
   //Delete a filter associated with a user.
-  res.end('End');
+  knex('filters').where({id : req.params.filterID}).del()
+  .then(function(err, data) {
+    if(!checkErr(res, err)) {
+      res.send('success');
+    }
+  });
 });
 
 
