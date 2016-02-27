0d7d06334033c271b24ac996983f084d68fbcc58
LKHohn <lkhohn@gmail.com>
2016-02-27 12:27:13 -0700

    cors update, app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var corsOptions = {
-  origin: 'http://localhost:8100'
+  origin: '*'
 };
 app.use(cors(corsOptions));
 
